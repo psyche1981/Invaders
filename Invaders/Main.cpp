dae878c5151c21c63095f461f15ccf32f69b66ac
Simon Jones <spjones1981@gmail.com>
2018-07-23 20:10:03 +0100

    Project up and running

---
+++ Invaders/Main.cpp
@@ -0,0 +1,47 @@
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+int main()
+{
+	// Create the main window
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+	// Load a sprite to display
+	sf::Texture texture;
+	if (!texture.loadFromFile("NS.png"))
+		return EXIT_FAILURE;
+	sf::Sprite sprite(texture);
+	sprite.move(400.0f, 300.0f);
+	// Create a graphical text to display
+	sf::Font font;
+	if (!font.loadFromFile("space_invaders.ttf"))
+		return EXIT_FAILURE;
+	sf::Text text("Hello SFML", font, 50);
+	sf::Color red(255, 0, 0, 255);
+	text.setFillColor(red);
+	// Load a music to play
+	//sf::Music music;
+	//if (!music.openFromFile("nice_music.ogg"))
+	//	return EXIT_FAILURE;
+	//// Play the music
+	//music.play();
+	// Start the game loop
+	while (window.isOpen())
+	{
+		// Process events
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// Close window: exit
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		// Clear screen
+		window.clear();
+		// Draw the sprite
+		window.draw(sprite);
+		// Draw the string
+		window.draw(text);
+		// Update the window
+		window.display();
+	}
+	return EXIT_SUCCESS;
+}
